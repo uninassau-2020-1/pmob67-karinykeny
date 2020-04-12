2197c3462382a2d4aedbed2fd85ae6aaf193b46f
karinykeny <karinykeny@gmail.com>
2020-04-12 20:51:21 -0300

    projeto inicial

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page{
+  length = 8;
+  valorDoCep: boolean = false;
+  msg: string = "";
+  endereco: any = {};
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  reset(cep) {
+    this.endereco = {};
+    this.valorDoCep = false;
+  }
+
+  validacao(cep) {
+    if (cep.length != this.length) {
+      this.valorDoCep = true;
+      this.msg = "O cep deve conter 8 númeos!";
+    }
+  }
+
+  consultarEndereco(cep) {
+    this.validacao(cep);
+    if ( this.valorDoCep == false) {
+      var urlApi = `http://viacep.com.br/ws/${cep}/json/`;
+     this.http.get(urlApi).subscribe(consulta => {
+      this.endereco = consulta;
+      cep = "";
+     });
+    }   
+  }
 
 }
