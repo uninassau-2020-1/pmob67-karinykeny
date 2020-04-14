acdacc123a040c790f35df2e980ac028117457da
kkc-at-002407643782 <kariny.dantas@hivecloud.com.br>
2020-04-14 13:11:51 -0300

    Alteração no formulario e melhorias visuais

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { IonInput } from '@ionic/angular';
+import { Button } from 'protractor';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,8 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page{
+
+  form: any = {cep: ""};
 
   length = 8;
   valorDoCep: boolean = false;
@@ -20,21 +24,25 @@ export class Tab1Page{
     this.valorDoCep = false;
   }
 
-  validacao(cep) {
-    if (cep.length != this.length) {
+  validacao(inf) {
+    if (inf.cep == null || inf.cep.length != this.length) {
       this.valorDoCep = true;
       this.msg = "O cep deve conter 8 númeos!";
     }
   }
 
   consultarEndereco( cep ) {
-    this.validacao(cep);
+    this.valorDoCep = false;
+    this.form = cep.form.value;
+    console.log("1 :" + this.form)
+    this.validacao(this.form);
     if ( this.valorDoCep == false) {
-      var urlApi = `http://viacep.com.br/ws/${cep}/json/`;
+      var urlApi = `http://viacep.com.br/ws/${this.form.cep}/json/`;
      this.http.get(urlApi).subscribe(consulta => {
       this.endereco = consulta;
-     });
-    }   
-  }
+    });
+    } 
+    this.form = " ";
+   }
 
 }
