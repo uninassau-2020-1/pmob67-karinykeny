b87894f6f074ea2fc366f643b4c8f76795e3942e
kkc-at-002407643782 <kariny.dantas@hivecloud.com.br>
2020-06-12 18:41:29 -0300

    SQLite

--- src/app/providers/databese.service.ts
+++ src/app/providers/databese.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
+import { NULL_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Injectable({
   providedIn: 'root'
@@ -8,24 +9,28 @@ export class DatabeseService {
 
   constructor(private sqlite: SQLite) { }
   
-  getBD() {
+  public getBD() {
     return this.sqlite.create({
-      name: 'data.db',
+      name: 'dbapp.db',
       location: 'default'
     });
   }
 
-  createData() {
+  public createBD() {
     return this.getBD()
     .then((db: SQLiteObject) => {
-      console.log(db);
+      this.createTable(db);
     })
     .catch(e => console.error(e));
   }
 
-  // createTable(db: SQLiteObject) {
-  //   return db.sqlBatch(
-
-  //   );
-  // }
+  private createTable(db: SQLiteObject) {
+    return db.sqlBatch(
+     ['CREATE TABLE IF NOT EXISTS endereco(id int IDENTITY(1,1) PRIMARY KEY, end varchar);'] 
+    ).then(() => {
+      console.log("Deu certo")
+    }).catch(e => {
+      console.error(e);
+    });
+  }
 }
